a7efbd545a006e698daece7a238ae9717221b9d6
IlyaKli <ilya.klimkin.0253@gmail.com>
2023-02-03 01:44:26 +0300

    Refactoring presentation

--- app/src/main/java/com/ilya/wallpaper/presentation/adapter/category/CategoryDiffCallback.kt
+++ app/src/main/java/com/ilya/wallpaper/presentation/adapter/category/CategoryDiffCallback.kt
@@ -1,13 +1,14 @@
 package com.ilya.wallpaper.presentation.adapter.category
 
 import androidx.recyclerview.widget.DiffUtil
+import com.ilya.wallpaper.domain.model.Category
 
-//class CategoryDiffCallback : DiffUtil.ItemCallback<CoinInfo>() {
-//    override fun areItemsTheSame(oldItem: CoinInfo, newItem: CoinInfo): Boolean {
-//        return oldItem.fromSymbol == newItem.fromSymbol
-//    }
-//
-//    override fun areContentsTheSame(oldItem: CoinInfo, newItem: CoinInfo): Boolean {
-//        return oldItem == newItem
-//    }
-//}
+class CategoryDiffCallback : DiffUtil.ItemCallback<Category>() {
+    override fun areItemsTheSame(oldItem: Category, newItem: Category): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: Category, newItem: Category): Boolean {
+        return oldItem == newItem
+    }
+}
